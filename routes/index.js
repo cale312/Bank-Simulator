31ca935b4ed6aa888f30e404da3c451767b7751a
caleoasin312 <zikisacalesogaxa@gmail.com>
2017-11-12 13:36:34 +0200

    Added functionality for checking if the user us logged in or not;

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ const express = require('express');
 const session = require('express-session');
 var router = express.Router();
 
+const userModel = require('../models/user.model');
+
 router.get('/', (req, res) => {
-    (!req.session.username) ? res.render('index', { title: 'Home' }) : res.redirect('/account/' + req.session.username);
+    if (!req.session.username) {
+        res.render('index', { title: 'Home'});
+    } else {
+        userModel
+            .findOne({
+                username: req.session.username
+            })
+            .then((user) => {
+                if (user.role === 'user') {
+                    res.redirect('/account/' + req.session.username);
+                } else {
+                    res.redirect('/bank');
+                }
+            })
+    }
 });
 
 router.post('/', (req, res) => {
